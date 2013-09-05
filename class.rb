eb506a33b27c5ee260110a15efa060c809994d45
Greg Hild <grhild@gmail.com>
2013-09-05 12:49:55 -0500

    Put your commit message here

--- class.rb
+++ class.rb
@@ -1,1 +1,12 @@
-asfkasfkj
+5.send(:*, 5)
+"omg".send(:upcase)
+['a', 'b', 'c'].send(:at, '1')
+['a', 'b', 'c'].send(:insert, '2', 'o', 'h', 'n', 'o')
+{}.send(:size)
+{character: "Mario"}.send(:has_key?, "character")
+
+6 - 32
+{html: true, json: false}.keys()
+"Makersquare"*6
+"Makersquare".split("a")
+['alpha', 'beta'].[](3)
